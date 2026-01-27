1432cc76925f972aaf577387a3ac23b5d72757fa
Jamshiddin <jbm050690@gmail.com>
2026-01-27 15:01:13 +0500

    vite config production uchun sozladim

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,23 +1,31 @@
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 
-export default defineConfig({
-  server: {
-    // open: true,
-    host: true,
-    port: 3002,
-  },
-  build: {
-    target: "esnext",
-    chunkSizeWarningLimit: 2048,
-  },
-  resolve: {
-    alias: {
-      "@app": path.resolve(__dirname, "./src/app"),
-      "@css": path.resolve(__dirname, "./src/css"),
-      "@lib": path.resolve(__dirname, "./src/library"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_PATH || "/",
+
+    server: {
+      host: true,
+      port: 3002,
     },
-  },
-  plugins: [eslintPlugin()],
+
+    build: {
+      target: "esnext",
+      chunkSizeWarningLimit: 2048,
+    },
+
+    resolve: {
+      alias: {
+        "@app": path.resolve(__dirname, "./src/app"),
+        "@css": path.resolve(__dirname, "./src/css"),
+        "@lib": path.resolve(__dirname, "./src/library"),
+      },
+    },
+
+    plugins: [eslintPlugin()],
+  };
 });
