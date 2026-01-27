fae8c239bc0a5e8b0c3e4a8374f337cf2e7e91c1
Jamshiddin <jbm050690@gmail.com>
2026-01-27 15:28:11 +0500

    server port to'g'rilandi

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ import { createServer } from "http";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 /* =======================
    HTTP SERVER & SOCKET.IO
 ======================= */
